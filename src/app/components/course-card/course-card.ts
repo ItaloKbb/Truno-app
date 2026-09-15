09bf76127a6ec71d0734581b0bd7223e0b9aaab9
Bruno Mocelin <brunohenriquemocelindossantos@gmail.com>
2026-09-15 17:13:38 -0300

    Run: ng g c components/navbar ng g c components/course-card

---
+++ src/app/components/course-card/course-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-course-card',
+  styleUrl: './course-card.css',
+  templateUrl: './course-card.html',
+})
+export class CourseCard {}
