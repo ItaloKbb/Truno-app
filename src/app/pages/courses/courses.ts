5ece3febfaeef9dbd10602185f19c0b23bcca05d
italoKbb <italohyu122@gmail.com>
2026-09-17 18:20:13 +0000

    Remove unused courses and detail components, including their styles and tests

--- src/app/pages/courses/courses.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-courses',
-  styleUrl: './courses.css',
-  templateUrl: './courses.html',
-})
-export class Courses {}
--- src/app/pages/detail/detail.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-detail',
-  styleUrl: './detail.css',
-  templateUrl: './detail.html',
-})
-export class Detail {}
